ec11a0437d74fd0d70d55253282f9dabcd9372aa
LukasRandecker <163838034+LukasRandecker@users.noreply.github.com>
2024-03-23 16:27:32 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function(){
-    document.getElementById("main_text_box").style.animation ="slideInL_R 1s ease-in-out" ; 
-  };
-
-window.onload= function(){
-    document.getElementById('main_text_box2').style.animation="slideInR_L 1s ease-in-out"; 
-}
